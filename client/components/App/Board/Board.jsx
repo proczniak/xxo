aaf476da17989e7f432754f27f75489a843fb7ae
Piotr Roczniak <proczniak@gmail.com>
2016-03-20 21:22:45 +0100

    Fixed what I just broke. (data not ready for component render)

--- client/components/App/Board/Board.jsx
+++ client/components/App/Board/Board.jsx
@@ -5,18 +5,11 @@ Board = React.createClass({
 
     var bD = Boards.findOne(); /** bD stands for boardData */
 
-    if (bD.A1 || bD.A2 || bD.A3 || bD.B1 || bD.B2 || bD.B3 || bD.C1 || bD.C2 || bD.C3) boardClear = false;
-    else boardClear = true;
-
- //   console.log('Board.jsx. getMeteorData var boardData: ' + boardData);
-
-
     if (!!bD) {
       return {
         boardId: bD._id,
         opponent: this.getOpponentName(bD),
-        moveToken: bD.moveToken,
-        boardClear: boardClear
+        moveToken: bD.moveToken
       }
     }
     else {
@@ -26,34 +19,20 @@ Board = React.createClass({
       }
     }
   },
-  getInitialState: function() {
-    return {boardCleared: false};
-  },
-  componentDidMount: function(){
-
-  },
-
-  componentDidUpdate: function(){
-    if (this.data.boardClear) sAlert.warning("Board cleared", {effect: 'genie'});
-  },
 
   getOpponentName: function (boardData) {
- //   console.log('getOpponenName invoked, this.userId: ' + Meteor.userId());
     if (boardData.player1 === Meteor.userId()) return boardData.p2Name
     else return boardData.p1Name
   },
 
   handleClearBoardClick: function (event) {
     Meteor.call('clearBoard');
-    this.setState({boardCleared: true});
   },
 
   render() {
 
     if (this.data.moveToken === Meteor.userId()) var moveTokenMsg = "Twój ruch"
     else var moveTokenMsg = "Ruch przeciwnika"
-
-    console.log("moveToken: " +this.data.moveToken);
 
     if (this.data.boardId != "none") {
       /** Ugly hack but without it component renders
@@ -62,8 +41,6 @@ Board = React.createClass({
        * Spent way too much time to isolate the problem and have no time to solve
        * it pretty way.
        */
-
-      console.log("this.data.boardId: " +this.data.boardId);
 
       return (
         <div className="row">
--- install-packages.sh
+++ install-packages.sh
@@ -1,4 +1,4 @@
 npm install expect
 npm install mocha
-meteor add accounts-password accounts-ui aldeed:collection2 blaze-html-templates dburles:collection-helpersecmascript es5-shim jquery juliancwirko:s-alert juliancwirko:s-alert-genie kadira:flow-router kadira:react-layout meteor-base mobile-experience mongo msavin:mongol ongoworks:security react session standard-minifiers tracker twbs:bootstrap juliancwirko:s-alert juliancwirko:s-alert-genie
+meteor add accounts-password accounts-ui aldeed:collection2 blaze-html-templates dburles:collection-helpers ecmascript es5-shim jquery juliancwirko:s-alert juliancwirko:s-alert-genie kadira:flow-router kadira:react-layout meteor-base mobile-experience mongo msavin:mongol ongoworks:security react session standard-minifiers tracker twbs:bootstrap
 meteor remove autopublish insecure
