7714a66dac3c030dbb48497d4e60a9b1cd6c0c5d
Piotr Roczniak <proczniak@gmail.com>
2016-03-21 14:41:52 +0100

    Added installer for Daniel

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 Available online at (http://sztos.roczniak.net:3000)
 
-
-##Prerequsites
-
-install needed packages with the provided "install-packages.sh" script.
+## Install
+To install and run the game run following command:
+'''
+curl https://raw.githubusercontent.com/proczniak/xxo/master/install.sh | sh
+'''
+If you're lucky, (Mac ppl usualy are) you kshould be up and running in a few minutes.
--- install-packages.sh
+++
@@ -1,4 +0,0 @@
-npm install expect
-npm install mocha
-meteor add accounts-password accounts-ui aldeed:collection2 blaze-html-templates dburles:collection-helpers ecmascript es5-shim jquery juliancwirko:s-alert juliancwirko:s-alert-genie kadira:flow-router kadira:react-layout meteor-base mobile-experience mongo msavin:mongol ongoworks:security react session standard-minifiers tracker twbs:bootstrap
-meteor remove autopublish insecure
